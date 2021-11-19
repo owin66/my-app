92931d4139bb6dc4a0754e1de1564db0e557c3f5
owin66 <78900270+owin66@users.noreply.github.com>
2021-11-19 11:14:54 +0200

    fix bag

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,7 +6,7 @@ import c from './Profile.module.css'
 const Profile = () => {
    return ( 
    <div className={c.content}>
-    <img alt='logo main' src='https://pbs.twimg.com/profile_banners/2577526642/1487186854/1500x500'></img>
+    <img alt='logo main' src='https://sun9-21.userapi.com/impg/aGCKfh1kMNqPwZXqT7WgCieHdUu_sume3GBn6Q/Of5KfPftfss.jpg?size=1500x500&quality=96&sign=dd0b3c8b12e826a98225e72ac634ed22&type=album'></img>
   <div>
     ava + description
   </div>
