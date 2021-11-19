80db2bbd1c8012bfc930566173a60a73ab3ecc6c
owin66 <78900270+owin66@users.noreply.github.com>
2021-11-19 12:49:49 +0200

    Add Dialogs.jsx

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 import Header from './components/Header/Header';
 import Navigation from './components/Navigation/Navigation';
 import Profile from './components/Profile/Profile';
-
-
+import Dialogs from "./components/Dialogs/Dialogs";
 
 
 const App = () => {
-  return (
-    <div className='wrapper'>
-      <Header />
-      <Navigation />
-      <Profile />
-    </div>
-  );
+    return (
+        <div className='wrapper'>
+            <Header/>
+            <Navigation/>
+            <div className="app-wrapper-content">
+                <Dialogs/>
+            </div>
+            {/*<Profile />*/}
+        </div>
+    );
 }
 
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import c from './Profile.module.css'
 
 const Profile = () => {
    return ( 
-   <div className={c.content}>
+   <div>
     <img alt='logo main' src='https://sun9-21.userapi.com/impg/aGCKfh1kMNqPwZXqT7WgCieHdUu_sume3GBn6Q/Of5KfPftfss.jpg?size=1500x500&quality=96&sign=dd0b3c8b12e826a98225e72ac634ed22&type=album'></img>
   <div>
     ava + description
